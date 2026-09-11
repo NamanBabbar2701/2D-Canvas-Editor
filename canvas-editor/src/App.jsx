2e35225726f0f2996a3afe989e0652b55f9d2f0a
NamanBabbar2701 <MITU22BTCS0467@students.mituniversity.edu.in>
2026-09-11 18:47:45 +0530

    feat: add canvas creation and firebase integration

--- canvas-editor/src/App.jsx
+++ canvas-editor/src/App.jsx
@@ -1,28 +1,59 @@
 import "./App.css";
+import {db} from "./firebase.js"
+import { collection, addDoc } from "firebase/firestore";
+import {Routes, Route, useNavigate} from "react-router-dom";
+import Canvas from "./pages/Canvas.jsx"
 
 function App(){
+
+  const createNewCanvas = async () => {
+      try{
+        const canvasRef = await addDoc(collection(db, "canvases"), {
+          name: "Untitled Canvas",
+          createdAt: new Date(),
+        });
+
+        console.log("Canvas created: ", canvasRef.id);
+
+        navigate(`/canvas/${canvasRef.id}`);
+      }catch (error) {
+        console.error("Error creating canvas:", error);
+      }
+  };
+
+  const navigate = useNavigate();
+
   return(
-    <div className="home"> 
-    {/* Navigation */}
-      <nav className="navbar">
-        <h2 className="logo">Canvas Editor</h2>
-      </nav>
+    <Routes>
+      <Route 
+        path="/"
+        element={
+          <div className="home"> 
+          {/* Navigation */}
+            <nav className="navbar">
+              <h2 className="logo">Canvas Editor</h2>
+            </nav>
 
-      {/* Hero Section */}
-      <main className="hero">
-        <h1>
-            Create. Draw. <span>Edit.</span>
-        </h1>
+            {/* Hero Section */}
+            <main className="hero">
+              <h1>
+                  Create. Draw. <span>Edit.</span>
+              </h1>
 
-        <p>
-          A simple 2D canvas editor for creating and editing ideas.
-        </p>
+              <p>
+                A simple 2D canvas editor for creating and editing ideas.
+              </p>
 
-        <button className="create-button">
-          + Create New Canvas
-        </button>
-      </main>
-    </div>
+              <button className="create-button" onClick={createNewCanvas}>
+                + Create New Canvas
+              </button>
+            </main>
+          </div>
+        }
+      />
+
+      <Route path="/canvas/:canvasId" element={<Canvas />} />
+    </Routes>
   )
 }
 
---
+++ canvas-editor/src/pages/Canvas.jsx
@@ -0,0 +1,10 @@
+function Canvas() {
+    return(
+        <div>
+            <h1>Canvas Editor</h1>
+            <p>This is your canvas page</p>
+        </div>
+    );
+}
+
+export default Canvas;
